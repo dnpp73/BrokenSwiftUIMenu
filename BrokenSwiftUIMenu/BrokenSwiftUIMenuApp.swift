9064adc104169669a645eed022ada2d088658587
Yusuke Sugamiya <yusuke.dnpp@gmail.com>
2026-04-16 10:04:09 +0900

    Initial Commit

---
+++ BrokenSwiftUIMenu/BrokenSwiftUIMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokenSwiftUIMenuApp.swift
+//  BrokenSwiftUIMenu
+//
+//  Created by dnpp on 2026/04/16.
+//
+
+import SwiftUI
+
+@main
+struct BrokenSwiftUIMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
